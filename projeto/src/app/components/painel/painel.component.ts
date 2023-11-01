25ad200a4b5ae903ff54b19760da4e2f42c9c103
Kauan <kauansbflu@gmail.com>
2023-11-01 16:01:46 -0300

    configurando ts

--- projeto/src/app/components/painel/painel.component.ts
+++ projeto/src/app/components/painel/painel.component.ts
@@ -8,11 +8,30 @@ import { AuthService } from 'src/app/services/AuthService';
   styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent {
+  role: string = '';
+  canShowAluno: boolean = false;
+  canShowAdmin: boolean = false;
+  canShowProfessor: boolean = false;
 
   constructor(private authService: AuthService,private router: Router) {}
 
-deslogar() {
-  this.authService.logout();
-  this.router.navigate(['/login'])
-}
-}
+  deslogar() {
+    this.authService.logout();
+    this.router.navigate(['/login'])
+  }
+
+  showModule = () => {
+    if (this.authService.getUserRole() === 'aluno'){
+        this.canShowAluno = true;
+    } else if (this.authService.getUserRole() === 'admin'){
+        this.canShowAdmin = true;
+    }else if (this.authService.getUserRole() === 'Professor'){
+      this.canShowProfessor = true;
+    }
+
+    return this.canShowAluno, this.canShowAdmin, this.canShowProfessor;
+  }
+  
+
+  }
+
