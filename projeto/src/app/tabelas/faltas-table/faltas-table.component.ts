b95582d5c6255a38bb3608c55e43c3536cd67dbb
Kauan <kauansbflu@gmail.com>
2023-11-06 11:39:24 -0300

    comentando simulação

--- projeto/src/app/tabelas/faltas-table/faltas-table.component.ts
+++ projeto/src/app/tabelas/faltas-table/faltas-table.component.ts
@@ -6,11 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./faltas-table.component.scss']
 })
 export class FaltasTableComponent {
-  faltas = [
+
+  
+  faltas = [ //simulação
     { disciplina: 'Teste e Métricas', mes1: 5, mes2: 3, mes3: 2, mes4: 1, total: 11 },
     { disciplina: 'Laboratório de Inovação IV', mes1: 4, mes2: 2, mes3: 3, mes4: 1, total: 10 },
     { disciplina: 'Programação OO', mes1: 4, mes2: 2, mes3: 3, mes4: 1, total: 10 },
-    // Adicione mais disciplinas e faltas conforme necessário
   ];
+
   displayedColumns: string[] = ['disciplina', 'mes1', 'mes2', 'mes3', 'mes4', 'total'];
 }
